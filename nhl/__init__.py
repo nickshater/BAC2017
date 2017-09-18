949cbd5145eb15b6aff1d7143717ebd15071fbf3
William C. Kinsman <kinsman.will@gmail.com>
2017-09-18 09:29:53 -0400

    notification check

--- nhl/__init__.py
+++ nhl/__init__.py
@@ -1,15 +1,28 @@
 # -*- coding: utf-8 -*-
+"""
+888b    888 888    888 888          .d88888b.  8888888b.  8888888b.   .d8888b.  
+8888b   888 888    888 888         d88P" "Y88b 888  "Y88b 888  "Y88b d88P  Y88b 
+88888b  888 888    888 888         888     888 888    888 888    888 Y88b.      
+888Y88b 888 8888888888 888         888     888 888    888 888    888  "Y888b.   
+888 Y88b888 888    888 888         888     888 888    888 888    888     "Y88b. 
+888  Y88888 888    888 888         888     888 888    888 888    888       "888 
+888   Y8888 888    888 888         Y88b. .d88P 888  .d88P 888  .d88P Y88b  d88P 
+888    Y888 888    888 88888888     "Y88888P"  8888888P"  8888888P"   "Y8888P"  
 
+A package for scraping and manipulating NHL data
+
+AUTHOR: BAC
+DATE:   2017-18
+
+NOTES:  This __init__ file contains common information prints or string
+        conversions for simple data wrangling. Check modules for other
+        functionalities
+"""
 import os
-
 import pickle
+import oddscalculator as oc
 
-def print_teams():
-    """
-    return: list of teams
-    """
-    abbrevs = pickle.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'pickles','dict_names'),'rb'))
-    for k,v in abbrevs.items(): print(k + ": " + ','.join(v))
+########################### STRING CONVERSIONS ################################
 
 def team_given_abbrev(team_abbrev):
     """
@@ -35,3 +48,13 @@ def abbrev_given_team(team_name):
 #    @param: time, e.g. '12:01'
 #    return: seconds
 #    """
+
+############################ ODDS CONVERSIONS #################################
+
+
+############################ PRINT STATEMENTS #################################
+
+def print_teams():
+    abbrevs = pickle.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'pickles','dict_names'),'rb'))
+    for k,v in abbrevs.items(): print(k + ": " + v)
+
--- scrape_players.py
+++ scrape_players.py
@@ -36,8 +36,11 @@ def build_table(html_table,headers,guid,date,team,home_or_away):
 games = pickle.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'nhl','pickles','table_games.pickle'),'rb'))
 skaters = []
 goalies = []
+not_found = []
 progress = tqdm(total = games.shape[0])
+counter = 0
 for _,row in games.iterrows():
+    counter+=1
     progress.update()
     guid = row['guid']
     date = row['date']
@@ -49,6 +52,7 @@ for _,row in games.iterrows():
         url = 'https://www.hockey-reference.com/boxscores/' + row['guid'] + '.html'
         soup = BeautifulSoup(urlopen(url),'lxml')
     except:
+        not_found.append(url)
         print("URL not found: " + url)
         continue
         
@@ -58,6 +62,7 @@ for _,row in games.iterrows():
     away_skaters = soup.find("div",{"id":'all_' + away_team + '_skaters'})
     away_goalies = soup.find("div",{"id":'all_' + away_team + '_goalies'})
     if not all([home_skaters,home_goalies,away_skaters,away_goalies]):
+        not_found.append(url)
         print("Data missing: " + url)
     
     # get headers
@@ -67,9 +72,9 @@ for _,row in games.iterrows():
     headers_goalies = [i['data-stat'].lower() for i in headers_goalies.find_all('th')]
     
     headers_skaters.remove('ranker')
-    headers_skaters = ['team','home_away'] + headers_skaters
+    headers_skaters = ['guid','date','team','home_away'] + headers_skaters
     headers_goalies.remove('ranker')
-    headers_goalies = ['team','home_away'] + headers_goalies    
+    headers_goalies = ['guid','date','team','home_away'] + headers_goalies    
     
     # build dataframes from html
     df_home_skaters = build_table(home_skaters,headers_skaters,guid,date,home_team,'home')
@@ -84,14 +89,24 @@ for _,row in games.iterrows():
     goalies.append(df_away_goalies)
     
     # pause for scraping courtesy
-    time.sleep(0.5*float(random()))
+    #time.sleep(0.5*float(random()))
+    
+    # save every 20 runs
+    if counter%20==0:
+            with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'nhl','pickles','table_players.pickle'),'wb') as f:
+                pickle.dump(skaters,f)
+                pickle.dump(goalies,f)
+                pickle.dump(not_found,f)
+                pickle.dump(row,f)
 
-
-# pandas dataframe cleanup
+# pandas dataframe cleanup and pickle dump
 progress.close()
 df_skaters = pd.concat(skaters)
 df_skaters = df_skaters.sort_values('guid')
 df_skaters = df_skaters.reset_index(drop=True)
+df_skaters = df_skaters.replace('','NULL')
 df_goalies = pd.concat(goalies)
 df_goalies = df_goalies.sort_values('guid')
 df_goalies = df_skaters.reset_index(drop=True)
+df_goalies = df_goalies.replace('','NULL')
+
